bfd25a960a3d81716393ffeaae08e828adbc1e44
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-09-25 19:05:33 +0530

    Anecdotes and the backend, step4

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,6 +1,5 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
-import anecdoteService from '../services/anecdotes'
 import {
   setNotification,
   removeNotification,
@@ -13,8 +12,7 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
+    dispatch(createAnecdote(content))
     dispatch(setNotification(`you added '${content}'`))
     setTimeout(() => {
       dispatch(removeNotification())
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -18,7 +18,7 @@ const anecdoteSlice = createSlice({
         .map((anecdote) => (anecdote.id === id ? changedAnecdote : anecdote))
         .sort((a, b) => b.votes - a.votes)
     },
-    createAnecdote(state, action) {
+    appendAnecdote(state, action) {
       return [...state, action.payload].sort((a, b) => b.votes - a.votes)
     },
     setAnecdotes(state, action) {
@@ -27,12 +27,19 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const { addVote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
   return async (dispatch) => {
     const anecdotes = await anecdoteService.getAll()
     dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
   }
 }
 
